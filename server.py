8554bce3369d875ae0839b1f78ba0e7ed9f174dc
ArielWT <Ariel.ywt@gmail.com>
2025-10-02 00:11:15 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "¡Hola! 🚀 Tu servidor Flask AWServer está funcionando"
+    return "¡Hola! Tu servidor Flask AWServer funciona"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
